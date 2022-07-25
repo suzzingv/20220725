091361f6c44e18181fe7de488d681a05478a8a93
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-07-25 17:05:22 +0900

    초기 커밋.

---
+++ 20220725/20220725.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+char* GetStr(int size)
+{
+	char* str = (char*)malloc(size);
+	printf("문자열 입력: ");
+	gets(str);
+	return str;
+}
+int main(void)
+{
+	int size;
+	char* arr;
+
+	printf("문자열의 최대 길이: ");
+	scanf_s("%d", &size);
+	getchar();
+	arr = GetStr(size);
+
+	printf("%s \n", arr);
+	return 0;
+}
